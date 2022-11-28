7fed0d9b5ea6c2d5a2fb3de6714b9d7438033af6
Ryoya <109121569+ryoya310@users.noreply.github.com>
2022-11-29 08:39:12 +0900

    0.0.1

--- src/Components/calendar.tsx
+++ src/Components/calendar.tsx
@@ -1,29 +1,49 @@
 import React from 'react';
+import dayjs, { Dayjs } from 'dayjs'
+
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import styles from './index.module.scss';
 
 export default function CalendarX() {
 
+  const dateFormat = (date: string, format?: string) => {
+    format = (format === undefined) ? 'YYYY/MM/DD' : format
+    return dayjs(date).format(format)
+  }
+
   const [value, onChange] = React.useState(new Date())
 
-  const month_datas = {
-    '20221125': '123'
+  const date_json = {
+    '20221121': '123'
+  }
+  const DayContents = (props: any) => {
+    const { date, view } = props;
+    const dt = dateFormat(date, 'YYYYMMDD')
+    const dt_text: string = (date_json[dt]) ? date_json[dt] : ''
+    return <p>{dt_text}</p>
   }
 
-  const Tile = () => {
-    let tile = <></>;
-    return tile;
+  const onClickDay = (date: any) => {
+    console.log(date)
   }
+
   return <>
     <div className={styles.calendar}>
+
       <Calendar
-        onChange={onChange}
+        className={styles.calendar}
+        locale='ja-JP'
+        calendarType='US'
+        formatDay={(locale, date: any) => dateFormat(date, 'DD')}
         value={value}
         prev2Label={null}
         next2Label={null}
-        tileContent={<Tile />}
+        onChange={onChange}
+        onClickDay={onClickDay}
+        tileContent={DayContents}
       />
+
     </div>
   </>
 }
