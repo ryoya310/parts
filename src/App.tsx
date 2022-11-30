fc23e3b71e91d7c2d581a135e3fcd329e3cf8ed0
Ryoya <109121569+ryoya310@users.noreply.github.com>
2022-11-30 20:17:06 +0900

    0.0.5

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import Box from '@mui/material/Box';
+import dayjs from 'dayjs'
 import Calendar from "./Components/calendar/";
 
 function App() {
 
   // Calendar DB
   const datas: any = {
+    '20211121': {'text': '●', 'status': 'hol'},
+    '20211122': {'text': '■', 'status': ''},
     '20221021': {'text': '●', 'status': ''},
     '20221101': {'text': '●', 'status': ''},
     '20221121': {'text': '●', 'status': 'hol'},
@@ -16,12 +19,43 @@ function App() {
 
   // Day Click Event
   const onDayClick = (dt: Date) => {
-    console.log("callback")
+
+    const date = dayjs(dt).format("YYYY.MM.DD")
+    console.log(date)
   }
 
   return <>
-    <Box sx={{ width: 400, margin: "auto" }}>
-      <Calendar datas={datas} onDayClick={onDayClick} />
+
+    <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+
+      <Box sx={{ width: 320, margin: "20px auto" }}>
+        <Calendar
+          lang='ja'
+          no={-1}
+          datas={datas}
+          isNavi={false}
+          onDayClick={onDayClick}
+        />
+      </Box>
+
+      <Box sx={{ width: 320, margin: "20px auto" }}>
+        <Calendar
+          lang='ja'
+          no={0}
+          datas={datas}
+          isNavi={true}
+          onDayClick={onDayClick}
+        />
+      </Box>
+
+      <Box sx={{ width: 320, margin: "20px auto" }}>
+        <Calendar
+          lang='en'
+          no={1}
+          datas={datas}
+          onDayClick={onDayClick}
+        />
+      </Box>
     </Box>
   </>
 }
--- src/Components/calendar/index.tsx
+++ src/Components/calendar/index.tsx
@@ -6,14 +6,23 @@ import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 
 type Props = {
-  lang?: 'ja' | 'en' // 日本語 | 英語
-  no?: number // 現在月から指定
-  datas?: any // カレンダーのデータ
-  onDayClick?(dt: Date): void // 日付イベント
+  // 日本語 | 英語
+  lang?: 'ja' | 'en'
+  // 現在月から指定
+  no?: number
+  // カレンダーのデータ
+  datas?: any
+  // ナビゲーション表示
+  isNavi?: boolean
+  // 日付イベント
+  onDayClick?(dt: Date): void
 }
 export default function Calendar(props: Props) {
 
-  let { lang, no, datas, onDayClick } = props
+  let { lang, no, datas, isNavi, onDayClick } = props
+
+  // ナビゲーション表示
+  isNavi = (isNavi !== undefined) ? isNavi : true
 
   // 曜日表記
   let weeks: any = [];
@@ -55,11 +64,13 @@ export default function Calendar(props: Props) {
     )
   }
 
-  // 月の始～至
+  // 月の始
   const stDate = new Date(value.getFullYear(), value.getMonth(), 1)
   const st = (stDate.getDay() >= 6) ? 1 : stDate.getDay() * -1
+  // 月の至
   const enDate = new Date(value.getFullYear(), value.getMonth() + 1, 0)
-  const en = (enDate.getDay() === 6) ? enDate.getDate() - 0 : enDate.getDate() + 6 - enDate.getDay()
+  let en = (enDate.getDay() === 6) ? enDate.getDate() - 0 : enDate.getDate() + 6 - enDate.getDay()
+  if (enDate.getDate() === en) { en += 7 }
 
   // 月の日
   let Days = [];
@@ -72,13 +83,9 @@ export default function Calendar(props: Props) {
     // db
     if (datas[dateFormat(cuDate, 'YYYYMMDD')]) {
       // 休日
-      if (dtJSON.status === 'hol') {
-        cls.push(styles.hol)
-      }
+      if (dtJSON.status === 'hol') { cls.push(styles.hol) }
       // メモ
-      if (dtJSON.text) {
-        add = dtJSON.text
-      }
+      if (dtJSON.text) { add = dtJSON.text }
     }
     // 今日
     if (`${dateFormat(cuDate, 'YYYYMMDD')}` === `${dateFormat(toDay, 'YYYYMMDD')}`) { cls.push(styles.today) }
@@ -95,7 +102,6 @@ export default function Calendar(props: Props) {
           type='button'
           onClick={
             () => {
-              console.log(cuDate)
               if (onDayClick) {
                 onDayClick(cuDate)
               }
@@ -105,34 +111,38 @@ export default function Calendar(props: Props) {
           <span>
             {cuDate.getDate()}
           </span>
-          <div>
-            {add}
-          </div>
         </button>
+        <div>
+          {add}
+        </div>
       </div>
     )
   }
 
   return <>
-    <div className={styles.calendar}>
+    <div className={`calendar ${styles.calendar} ${styles.bg_gray}`}>
 
-      <div className={styles.navigation}>
-        <div className={styles.prev}>
-          <button type='button' onClick={onPrev}>
-            <ArrowBackIosNewIcon />
-          </button>
-        </div>
-        <div className={styles.current}>
-          <button type='button' onClick={onCurrent}>
-            {dateFormat(value, 'YYYY/MM')}
-          </button>
-        </div>
-        <div className={styles.next}>
-          <button type='button' onClick={onNext}>
-            <ArrowForwardIosIcon />
-          </button>
-        </div>
-      </div>
+      {
+        (isNavi)
+          ? <div className={styles.navigation}>
+              <div className={styles.prev}>
+                <button type='button' onClick={onPrev}>
+                  <ArrowBackIosNewIcon />
+                </button>
+              </div>
+              <div className={styles.current}>
+                <button type='button' onClick={onCurrent}>
+                  {dateFormat(value, 'YYYY/MM')}
+                </button>
+              </div>
+              <div className={styles.next}>
+                <button type='button' onClick={onNext}>
+                  <ArrowForwardIosIcon />
+                </button>
+              </div>
+            </div>
+          : <></>
+      }
 
       <div className={styles.views}>
         <div className={styles.weekdays}>
